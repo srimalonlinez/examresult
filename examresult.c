9415365b41c4a1f64a906f730bd94ddb3fe8964a
Srimal Tharupathi <120041802+srimalonlinez@users.noreply.github.com>
2022-12-10 23:11:56 +0530

    Add files via upload

---
+++ examresult.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int final;
+
+    printf("Enter Your Marks :");
+    scanf("%d",&final);
+
+    if (final>=60)
+    {
+        printf("Congratulations,you have passed the exam...\n");
+    }
+    printf("Thank you for your participate for all the participant....");
+
+    return 0;
+    
+}
